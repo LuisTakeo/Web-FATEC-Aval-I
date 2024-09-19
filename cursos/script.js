00fd771bc0bd534547f2f77fb5d389433290cd41
Luis Takeo <110575504+LuisTakeo@users.noreply.github.com>
2024-09-19 15:47:04 -0300

    Alterações na responsividade

--- cursos/script.js
+++ cursos/script.js
@@ -5,3 +5,17 @@ menuHamburger.addEventListener("click", () => {
   navList.classList.toggle("display-show");
   console.log("clicked");
 });
+
+
+function aviso(){
+  email = document.getElementById('email-value').value
+  if (email.includes('@gmail.com') || email.includes('@outlook.com') || email.includes('@fatec.sp.gov.br')) {
+      alert("Email cadastrado com sucesso.")
+  }else{
+      alert("Email invalido.")
+  }
+}
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", aviso);
